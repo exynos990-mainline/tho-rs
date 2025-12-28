f9693409f0bb80f2665eb7bed5cac54797887225
Umer Uddin <umer.uddin@mentallysanemainliners.org>
2025-12-28 15:04:22 +0000

    tho-rs: usb: Fix various mistakes
    Fixes return types and general usb handling.
    
    Signed-off-by: Umer Uddin <umer.uddin@mentallysanemainliners.org>

--- src/usb.rs
+++ src/usb.rs
@@ -1,64 +1,61 @@
 use std::time::Duration;
 
-// Waits for a download mode device and returns a device handle, or None if an error occurs.
-pub fn initialise_usb() -> Option<rusb::DeviceHandle<rusb::GlobalContext>>{
+pub type USBResult =
+    core::result::Result<rusb::DeviceHandle<rusb::GlobalContext>, Box<dyn core::error::Error>>;
+pub type Result<T> = core::result::Result<T, Box<dyn core::error::Error>>;
+
+/// Waits for a download mode device and returns a device handle, or None if an error occurs.
+pub fn initialise_usb() -> USBResult {
     loop {
-        for device in rusb::devices().unwrap().iter() {
-            let device_desc = device.device_descriptor().unwrap();
+        for device in rusb::devices()?.iter() {
+            let device_desc = device.device_descriptor()?;
 
             if device_desc.vendor_id() == 0x04E8 && device_desc.product_id() == 0x685D {
                 println!("Found download mode device");
-                match device.open() {
-                    Ok(device_handle) => {
-                        if let Err(e) = device_handle.claim_interface(1) {
-                            println!("Failed to claim interface: {e:?}");
-                            return None;
-                        }
-                        return Some(device_handle);
-                    },
-                    Err(e) => {
-                        println!("Failed to open device: {e:?}");
-                        return None;
-                    }
-                }
+
+                let handle = device
+                    .open()
+                    .map_err(|e| format!("Failed to open device: {e:?}"))?;
+                handle.claim_interface(1)?;
+
+                return Ok(handle);
             }
         }
     }
 }
 
-// Does a USB bulk transfer, returns true on success, false on failure.
-pub fn usb_bulk_transfer(device_handle: &mut rusb::DeviceHandle<rusb::GlobalContext>, data: Vec<u8>) -> bool{
+/// Does a USB bulk transfer, returns true on success, false on failure.
+pub fn usb_bulk_transfer(
+    device_handle: &mut rusb::DeviceHandle<rusb::GlobalContext>,
+    data: &[u8],
+) -> Result<()> {
     match device_handle.write_bulk(2, &data, Duration::from_secs(5)) {
         Ok(bytes_written) => {
             if bytes_written == data.len() {
-                return true;
+                Ok(())
             } else {
-                return false;
+                Err("bytes written != data.len()".into())
             }
-        },
-        Err(e) => {
-            println!("Bulk transfer error: {e:?}");
-            return false;
         }
+        Err(e) => Err(format!("Bulk transfer error: {e:?}").into()),
     }
 }
 
-// Does a USB bulk read, returns a tuple of (status, bytes).
-pub fn usb_bulk_read(device_handle: &mut rusb::DeviceHandle<rusb::GlobalContext>) -> (bool, Vec<u8>) {
+/// Does a USB bulk read, returns a tuple of (status, bytes).
+pub fn usb_bulk_read(
+    device_handle: &mut rusb::DeviceHandle<rusb::GlobalContext>,
+) -> Result<Vec<u8>> {
     let mut buffer: Vec<u8> = vec![0u8; 512];
 
     match device_handle.read_bulk(0x81, &mut buffer, Duration::from_secs(5)) {
         Ok(bytes_read) => {
             if bytes_read > 0 {
                 buffer.truncate(bytes_read);
-                return (true, buffer);
+                Ok(buffer)
             } else {
-                return (false, buffer);
+                Err("bytes_read <= 0".into())
             }
-        },
-        Err(e) => {
-            println!("Bulk read error: {e:?}");
-            return (false, buffer);
         }
+        Err(e) => Err(format!("Bulk read error: {e:?}").into()),
     }
 }
